53852d8bc0bde90eb202f95bc758c3754a7649e6
m-woo <mark.chi.woo@gmail.com>
2017-04-06 20:02:52 -0700

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -15,13 +15,14 @@ Rails.application.routes.draw do
 
 	root 'main#index'
 	
-  # get 'main/index'
+  get 'main/about'
 
  	get "login" => 'sessions#new'
  	post "login" => 'sessions#create'
  	get "logout" => 'sessions#destroy'
 
  	resources :user
+  resources :map
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
