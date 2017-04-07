1adf5891c1d18a89e5ab4e6458b5748ea3ead1ee
m-woo <mark.chi.woo@gmail.com>
2017-04-07 09:00:55 -0700

    google maps api

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,463 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+//Geolocator
+var x = document.getElementById("demo");
+
+
+//Google Maps API
+
+
+var script = document.createElement('script');
+
+
+function initMap() {
+
+// var Seattle = {lat: 47.6062, lng: -122.3321};
+
+var map = new google.maps.Map(document.getElementById
+  ('map'), {
+  center: Seattle,
+  zoom: 12,
+  styles: 
+  [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#ffffff"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dadada"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#c9c9c9"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  }
+]
+});
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+    console.log(latlon)
+}
+
+
+  function initAutocomplete() {
+            
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 47.6062, lng: -122.3321},
+          zoom: 12,
+          styles: 
+  [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#ffffff"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dadada"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#c9c9c9"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  }
+]
+});
+    
+
+        // Create the search box and link it to the UI element.
+        var input = document.getElementById('pac-input');
+        var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+        // Bias the SearchBox results towards current map's viewport.
+        map.addListener('bounds_changed', function() {
+          searchBox.setBounds(map.getBounds());
+        });
+
+        var markers = [];
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+        searchBox.addListener('places_changed', function() {
+          var places = searchBox.getPlaces();
+
+          if (places.length == 0) {
+            return;
+          }
+
+
+          // For each place, get the icon, name and location.
+          var bounds = new google.maps.LatLngBounds();
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+            var icon = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+            };
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+              map: map,
+              icon: icon,
+              title: place.name,
+              position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          map.fitBounds(bounds);
+        });
+
+
+    	var locations = [
+		  ["<div class='text-center'>16015 Cleveland St, Redmond, WA 98052 <br>" + "<a href='/'>Info</a></div>", 47.6744982,  -122.1270461],
+		  ["<div class='text-center'>2121 6th Ave, Seattle, WA 98121 <br>" + "<a href='/'>Info</a></div>", 47.6153327, -122.3398556],
+		  ["<div class='text-center'>1000 8th Ave, Seattle, WA 98104 <br>" + "<a href='/'>Info</a></div>", 47.60855309999999, -122.3281102],
+		  ["<div class='text-center'>430 Eighth Ave N, Seattle, WA 98109 <br>" + "<a href='/'>Info</a></div>", 47.6231545, -122.3404723],
+		  ["<div class='text-center'>3249 Conkling Pl W, Seattle, WA 98119 <br>" + "<a href='/'>Info</a></div>", 47.648971, -122.369671],
+		  ["<div class='text-center'>124 N 103rd St, Seattle, WA 98133 <br>" + "<a href='/'>Info</a></div>", 47.7039909, -122.3564095],
+		  ["<div class='text-center'>6230 Corson Ave S, Seattle, WA 98108 <br>" + "<a href='/'>Info</a></div>", 47.5474529, -122.3214487],
+		  ["<div class='text-center'>10015 10th Ave SW, Seattle, WA 98146 <br>" + "<a href='/'>Info</a></div>", 47.51329399999999, -122.348066],
+		  ["<div class='text-center'>801 E Thomas St, Seattle, WA 98102 <br>" + "<a href='/'>Info</a></div>", 47.6209277, -122.3218396],
+		  ["<div class='text-center'>2100 Western Ave, Seattle, WA 98121 <br>" + "<a href='/'>Info</a></div>", 47.6114621, -122.3448254],
+		  ["<div class='text-center'>1301 4th Ave, Seattle, WA 98101 <br>" + "<a href='/'>Info</a></div>", 47.6082156, -122.335365],
+		  ["<div class='text-center'>1230 5th Ave, Seattle, WA 98101 <br>" + "<a href='/'>Info</a></div>", 47.6083581, -122.3334327],
+		  ["<div class='text-center'>Port Blakely, Bainbridge Island, WA 98110 <br>" + "<a href='/'>Info</a></div>", 47.6249, -122.5210],
+		  ["<div class='text-center'>3851 83rd Ave SE, Mercer Island, WA 98040 <br>" + "<a href='/'>Info</a></div>", 47.575213, -122.22868],
+		  ["<div class='text-center'>11660 Main St, Bellevue, WA 98005 <br>" + "<a href='/'>Info</a></div>", 47.6103263, -122.182936]
+		];
+
+		var infowindow = new google.maps.InfoWindow();
+
+		function placeMarker( loc ) {
+			var latLng = new google.maps.LatLng( loc[1], loc[2]);
+			var marker = new google.maps.Marker({
+			  position : latLng,
+			  map      : map
+			});
+			google.maps.event.addListener(marker, 'click', function(){
+			    infowindow.close(); // Close previously opened infowindow
+			    infowindow.setContent( "<div id='infowindow'>"+ loc[0] +"</div>");
+			    infowindow.open(map, marker);
+			});
+		}
+		 
+		for(var i=0; i<locations.length; i++) {
+		placeMarker( locations[i] );
+		} 
+}   
+
+
